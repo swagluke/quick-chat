089083bcfa20c62856d357682c5573a41f681e59
Luke Zhang <zhangl@rose-hulman.edu>
2017-04-06 09:01:16 -0400

    placeholder html.

--- QuickChat/src/app/app.component.ts
+++ QuickChat/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(public authService: AuthService) {
