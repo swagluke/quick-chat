ad110d31eda30b7ada5b18d63d3537162fefd3bc
Luke Zhang <zhangl@rose-hulman.edu>
2017-04-18 10:28:36 -0400

    Almost Done.

--- QuickChat/src/app/post/post.component.ts
+++ QuickChat/src/app/post/post.component.ts
@@ -1,18 +1,65 @@
+import { PostService } from '../services/post.service';
+import { PostWithAuthor } from './../models/post.model';
+import { AuthService } from '../services/auth.service';
 import { Post } from '../models/post.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { MdSnackBar } from "@angular/material";
+
+export enum EditMode {
+  NoEditable = 0,
+  DisplayEditButton = 1,
+  Editing = 2,
+}
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css','../shared/common.scss']
+  styleUrls: ['./post.component.css', '../shared/common.scss']
 })
 export class PostComponent implements OnInit {
 
-  @Input() post: Post;
-  
-  constructor() { }
+  @Input() post: PostWithAuthor;
+  public editingMode: EditMode = EditMode.NoEditable;
+
+  constructor(private authService: AuthService,
+   private postService: PostService,
+   public snackBar: MdSnackBar) { }
 
   ngOnInit() {
+    if (this.post.authorKey == this.authService.currentUserUid) {
+      this.editingMode = EditMode.DisplayEditButton;
+    }
+  }
+
+  enableEditing(): void {
+    this.editingMode = EditMode.Editing;
+  }
+
+  remove(): void {
+    this.postService.remove(this.post.$key);
+    const snackBarRef = this.snackBar.open("Post removed", "UNDO", {
+      duration: 5000,
+    });
+
+    snackBarRef.onAction().subscribe(  () => { 
+      console.log("TODO: Put it back!");
+      const restoredPost = new Post();
+      restoredPost.postBody = this.post.postBody;
+      restoredPost.authorKey = this.authService.currentUserUid;
+      this.postService.update(this.post.$key, restoredPost);
+      this.snackBar.open("Post restored!", "", {
+        duration: 3000,
+      })
+     })
+  }
+
+  saveEdit(): void {
+    // Update the posts to the new value
+    this.editingMode = EditMode.DisplayEditButton;
+  }
+
+  cancelEdit(): void {
+    this.editingMode = EditMode.DisplayEditButton;
   }
 
 }
--- QuickChat/src/app/services/post.service.ts
+++ QuickChat/src/app/services/post.service.ts
@@ -1,6 +1,6 @@
 import { Author } from '../models/author.model';
 import { AuthorService } from './author.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Post, PostWithAuthor } from '../models/post.model';
 import { Injectable } from '@angular/core';
@@ -8,13 +8,46 @@ import * as firebase from "firebase";
 @Injectable()
 export class PostService {
   readonly postsPath = "posts";
+  readonly postBatchSize = 5;
+  private postIncrementStream: Subject<number>;
   public postsWithAuthorStream: Observable<PostWithAuthor[]>;
+  public hasMorePosts = true;
+
   constructor(private af: AngularFire, private authorService: AuthorService) {
-    const postsStream: FirebaseListObservable<Post[]> = this.af.database.list(this.postsPath);
+
+    this.postIncrementStream = new BehaviorSubject<number>(this.postBatchSize);
+    const numPostsStream: Observable<number> = this.postIncrementStream.scan(
+      (previousTotal: number, currentValue: number) => {
+        return previousTotal + currentValue;
+      });
+
+    const postsStream = numPostsStream.switchMap<number, Post[]>((numPosts: number) => {
+      return this.af.database.list(this.postsPath, {
+        query: {
+          limitToLast: numPosts,
+        }
+      })
+    });
+
+    // // To be deleted
+    // numPostsStream.subscribe( (numPosts: number) => {
+    //   console.log("Posts to display = " + numPosts);
+    // });
+
+    // const postsStream: FirebaseListObservable<Post[]> = this.af.database.list(this.postsPath, {
+    //   query: {
+    //     limitToLast: this.postBatchSize,
+    //   }
+    // });
+
+
     this.postsWithAuthorStream = Observable.combineLatest<PostWithAuthor[]>(
       postsStream,
       this.authorService.authorMapStream,
-      (posts: Array<Post>, authorMap: Map<string, Author>)  => {
+      numPostsStream,
+      (posts: Array<Post>, authorMap: Map<string, Author>, numPostsRequested: number) => {
+        this.hasMorePosts = numPostsRequested <= posts.length;
+
         const postsWithAuthors: PostWithAuthor[] = [];
         // console.log("posts:", posts);
         // console.log("authorMap:", authorMap);
@@ -36,5 +69,19 @@ export class PostService {
     // this._postsStream.push(post);
     console.log("TODO: push the post using the standard Firebase JS API");
     firebase.database().ref().child(this.postsPath).push(post);
+  }
+
+  displayMorePosts(): void {
+    this.postIncrementStream.next(this.postBatchSize);
+  }
+
+  remove(postKeyToRemove: string): void {
+    firebase.database().ref().child(this.postsPath).child(postKeyToRemove).remove();
+    //this.af.database.object(`/${this.postsPath}/${postKeyToRemove}`).remove();
+  }
+
+  update(postKey: string, post: Post) {
+    firebase.database().ref().child(this.postsPath).child(postKey).update(post);
+    //this.af.database.object(`/${this.postsPath}/${postKey}`).update(post);
   }
 }
